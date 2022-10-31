825a312cf582a3adbb6ba01900f35eb4277b0136
dknaik <naik96657dinesh@gmail.com>
2022-10-31 08:40:23 +0530

    dynamic input range component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [input, setInput]=useState();
+const [tags,setTags]=useState([])
+const [isKeyReleased, setIsKeyReleased] = useState(false);
+
+const onChange=(e)=>{
+const {value}=e.target;
+setInput(value)
+}
+
+const onKeyDown=(e)=>{
+
+  const {key}=e
+  const trimmedInput=input.trim()
+  console.log(key, trimmedInput);
+  if(key=='Enter' && trimmedInput.length && !tags.includes(trimmedInput)){
+    setTags([...tags,trimmedInput])
+    setInput('')
+
+  }
+   if (key === "Backspace" && !input.length && tags.length && isKeyReleased) {
+     e.preventDefault();
+     const tagsCopy = [...tags];
+     const poppedTag = tagsCopy.pop();
+
+     setTags(tagsCopy);
+     setInput(poppedTag);
+     setIsKeyReleased(false)
+   }
+}
+const onKeyUp=()=>{
+  setIsKeyReleased(true)
+}
+const deleteTag=(clickedInd)=>{
+  const res=tags.filter((val,i)=>i!=clickedInd)
+  setTags(res)
+}
+console.log("input", tags, isKeyReleased);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {tags.map((tag, i) => (
+        <div className="tag">
+          {tag}
+          <button onClick={() => deleteTag(i)}>X</button>
+        </div>
+      ))}
+      <input
+        value={input}
+        placeholder="Enter a tag"
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        onChange={onChange}
+       
+      />
     </div>
   );
 }
